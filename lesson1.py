a1c4161324ae22062583fa5053256d416c0aa0f4
Olzhas1307 <kalykovolzhas@gmail.com>
2022-11-19 11:32:24 +0600

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+x = int(input())
+if x >=3 and x<=10 or x>20 and x <=40:
+    print ('Ура')
+else:
+    print ('увы')
+
+
+
+
+name = 'синхрофазотрон'
+count = 0
+for bukva in name:
+    if bukva =='н':
+        count+=1
+print(count)
+ 
+
